4ab17647122112147303106603349aa2118723cd
Rafael <rafaelsantos.rsdn@gmail.com>
2020-05-02 19:06:57 -0300

    First Commit

---
+++ KissLogApp/KissLogApp/Config/DependencyInjectionConfig.cs
@@ -0,0 +1,25 @@
+﻿using KissLog;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KissLogApp.Config
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+        {
+            // register dependencies
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<ILogger>((context) =>
+            {
+                return Logger.Factory.Get();
+            });
+
+            return services;
+        }
+    }
+}
---
+++ KissLogApp/KissLogApp/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using KissLog;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace KissLogApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //ILogger, mesma nomenclatura de interface do log do asp.net
+        private readonly KissLog.ILogger _logger;
+
+        public HomeController(KissLog.ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ErrorSimulation()
+        {
+            _logger.Debug("Simulação de erro");
+            return View();
+        }
+
+        public IActionResult ExceptionSimulation()
+        {
+            throw new Exception("Algo horrível aconteceu aqui!");
+        }
+
+        public double Calculo()
+        {
+            int number = 0; 
+            return 5 / number;
+        }
+
+    }
+}
